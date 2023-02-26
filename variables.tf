6b7c8ad51fab06a5bcaea340f6ae3057f8aca986
diegosorace <dsorace@prismamp.com>
2023-02-26 02:30:18 -0300

    update

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  description = "The name of the policy"
+  type        = string
+  default     = ""
+}
+
+variable "role" {
+  description = "The path of the policy in IAM (tpl file)"
+  type        = string
+  default     = ""
+}
+
+variable "policy" {
+  description = "The path of the policy in IAM (tpl file)"
+  type        = string
+  default     = ""
+}
+
